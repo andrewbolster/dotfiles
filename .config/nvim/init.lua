a0b1f02de63998bca854d434ac3adbc185399a74
Andrew Bolster <andrew.bolster@gmail.com>
2025-05-06 09:51:12 +0100

    sync

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,7 +24,16 @@ require("lazy").setup({
       require "options"
     end,
   },
-
+  {
+  "oxfist/night-owl.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    -- load the colorscheme here
+    require("night-owl").setup()
+    vim.cmd.colorscheme("night-owl")
+  end,
+  },
   { import = "plugins" },
 }, lazy_config)
 
